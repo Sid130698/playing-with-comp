22900497194cd58cee71f96b3061441cec637617
Sid130698 <sidsingh130698@gmail.com>
2024-10-03 02:19:10 +0530

    created components for header footer and side nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WebHeaderComponent } from "./components/web-header/web-header.component";
+import { SideNavComponent } from "./components/side-nav/side-nav.component";
+import { MainBodyComponent } from "./components/main-body/main-body.component";
+import { WebFooterComponent } from "./components/web-footer/web-footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, WebHeaderComponent, SideNavComponent, MainBodyComponent, WebFooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
